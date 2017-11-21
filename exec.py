fc7a85525398e28fb3588bc4a96fcdee11ff4e8a
Pavel Perminov <pavel@perminov.im>
2017-11-21 23:43:12 +0500

    Version 0.6 Fully worked

--- exec.py
+++ exec.py
@@ -1,6 +1,11 @@
-from os import walk, path
+#!/usr/bin/env python3
+
+from os import walk, path, remove
+from time import time
 from hashlib import sha256
 from sqlite3 import connect
+import sys
+import logging
 
 
 def options_parser():
@@ -14,7 +19,7 @@ def options_parser():
 
 def filelist(folder):
     file_list = list()
-    for root, subdirs, file_name in walk(path.abspath(folder)):
+    for root, subfolders, file_name in walk(folder):
         if file_name:
             for fn in file_name:
                 current_object = root + '/' + fn
@@ -28,41 +33,55 @@ def hasher(filepath):
     return digest
 
 
-def bd(db_file=None):
-    def sub_routine():
+def database(db_file=None, names=None):
+    def create_tables():
         cursor = db.cursor()
-        cursor.execute(
-            'CREATE TABLE files (path TEXT NOT NULL, digest VARCHAR(65) NOT NULL)')
-        cursor.execute('CREATE INDEX files_index ON files (path, digest)')
+        for name in names:
+            cursor.execute(
+                """CREATE TABLE IF NOT EXISTS \"{0}\" (path TEXT NOT NULL, digest VARCHAR NOT NULL)""".format(name))
+            cursor.execute("""CREATE INDEX IF NOT EXISTS \"{0}\" ON \"{1}\" (digest)""".format(
+                name + "_index", name))
         db.commit()
         cursor.close()
     if db_file:
-        return connect(filename)
-    db = connect(':memory:')
-    sub_routine()
+        db = connect(db_file)
+    else:
+        db = connect(':memory:')
+    create_tables()
     return db
 
 
-def walker(filelist, db):
+def walker(filelist, db, table):
+    print(db)
     cursor = db.cursor()
     for path in filelist:
-        print(path)
         digest = hasher(path)
-        cursor.execute('INSERT INTO files (path, digest) VALUES (?,?)',[path,digest])
+        cursor.execute(
+            """INSERT INTO '{0}' (path, digest) VALUES (?,?)""".format(table), [path, digest])
     db.commit()
     cursor.close()
-    print(1)
-    print(db.execute('select * from files').fetchall())
-
 
 
 def main():
-    d = bd()
+    bin_path = path.abspath(path.dirname(__file__))
     folders = options_parser()
-    files1 = filelist(folders.folder1)
-    files2 = filelist(folders.folder2)
-    walker(files2,d)
-    # print (files2)
+    current_time = "table_" + str(int(time())) + ":"
+    path1 = path.abspath(folders.folder1)
+    path2 = path.abspath(folders.folder2)
+    table1 = current_time + path1
+    table2 = current_time + path2
+    # print([table1,table2])
+    db = database(bin_path + '/1.db', [table1, table2])
+    # sys.exit()
+    files1 = filelist(path1)
+    files2 = filelist(path2)
+    walker(files2, db, table2)
+    walker(files1, db, table1)
+    data = db.execute(
+        """select '{0}'.path as path1 from '{0}' join '{1}' on '{0}'.digest = '{1}'.digest""".format(table2, table1))
+    for filepath in data.fetchall():
+        remove(filepath[0])
+    new_files=
 
 
-main()
+print(main())
