49aae9e6aa735618d65d67821c3565fd63b8ad41
Pavel Perminov <user100@svd-ad01-ws7001.ad.ies-holding.com>
2017-11-22 16:54:41 +0500

    Added: * Logging * Pre-Module structure
    Changed:
    * bin_path declaration
    * hasher changed for missing link ignorance
    * walker now log path (just for debug)
    * walker now execute sql insertion only when hasher returns data
    * main renamed to files_deduplicator
    * in files_deduplicator changed way of variables declaration
    * all deleted files pathes now added to log
    * all breaks with Ctrl+C

--- exec.py
+++ exec.py
@@ -8,6 +8,12 @@ import sys
 import logging
 
 
+bin_path = path.abspath(path.dirname(__file__)) + "/"
+
+logging.basicConfig(filename=bin_path + str(time()) +
+                    ".log", level=logging.DEBUG)
+
+
 def options_parser():
     from argparse import ArgumentParser
     parser = ArgumentParser(usage=None)
@@ -28,9 +34,12 @@ def filelist(folder):
 
 
 def hasher(filepath):
-    with open(filepath, 'rb') as stream:
-        digest = sha256(stream.read()).hexdigest()
-    return digest
+    try:
+        with open(filepath, 'rb') as stream:
+            digest = sha256(stream.read()).hexdigest()
+        return digest
+    except:
+        return
 
 
 def database(db_file=None, names=None):
@@ -52,36 +61,38 @@ def database(db_file=None, names=None):
 
 
 def walker(filelist, db, table):
-    print(db)
     cursor = db.cursor()
     for path in filelist:
+        logging.info(path)
         digest = hasher(path)
-        cursor.execute(
-            """INSERT INTO '{0}' (path, digest) VALUES (?,?)""".format(table), [path, digest])
+        if digest:
+            cursor.execute(
+                """INSERT INTO '{0}' (path, digest) VALUES (?,?)""".format(table), [path, digest])
     db.commit()
     cursor.close()
 
 
-def main():
-    bin_path = path.abspath(path.dirname(__file__))
+def files_deduplicator():
+    global bin_path
+    current_time = "table_" + str(int(time())) + ":"
     folders = options_parser()
     current_time = "table_" + str(int(time())) + ":"
-    path1 = path.abspath(folders.folder1)
-    path2 = path.abspath(folders.folder2)
-    table1 = current_time + path1
-    table2 = current_time + path2
-    # print([table1,table2])
+    path1, path2 = [path.abspath(folders.folder1),
+                    path.abspath(folders.folder2)]
+    table1, table2 = [current_time + path1, current_time + path2]
+    files1, files2 = [filelist(path1), filelist(path2)]
     db = database(bin_path + '/1.db', [table1, table2])
-    # sys.exit()
-    files1 = filelist(path1)
-    files2 = filelist(path2)
     walker(files2, db, table2)
     walker(files1, db, table1)
     data = db.execute(
         """select '{0}'.path as path1 from '{0}' join '{1}' on '{0}'.digest = '{1}'.digest""".format(table2, table1))
     for filepath in data.fetchall():
+        logging.debug(filepath)
         remove(filepath[0])
-    new_files=
 
 
-print(main())
+if __name__ == "__main__":
+    try:
+        files_deduplicator()
+    except KeyboardInterrupt:
+        sys.exit()
